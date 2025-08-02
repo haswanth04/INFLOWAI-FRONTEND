2642d8274cce63a8ca817b363edb81a506072c74
haswanth <haswnth.chinni@gmail.com>
2025-08-03 05:15:26 +0530

    push 2

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/ask", {
+      const response = await fetch("https://inflowaibackend.onrender.com/ask", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
